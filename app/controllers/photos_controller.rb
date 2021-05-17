91c866cb48f616b5d035b692a99ccbbe818d1b3a
etrinklein <etrinklein@gmail.com>
2021-05-17 17:30:36 +0000

    80%

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -31,12 +31,18 @@ class PhotosController < ApplicationController
   end
 
   def update
+
     id = params.fetch("the_photo_id")
     photo = Photo.where({ :id => id }).at(0)
-    photo.caption = params.fetch("input_caption")
-    photo.image = params.fetch("input_image")
-    photo.save
 
-    redirect_to("/photos/#{photo.id}")
+    if photo.owner_id = session.fetch(":user_id")
+      photo.caption = params.fetch("input_caption")
+      photo.image = params.fetch("input_image")
+      photo.save
+
+      redirect_to("/photos/#{photo.id}")
+    else
+      redirect_to("/photos/#{photo.id}")
+    end
   end
 end
